1c68e82e71409b5fe09a6a9af6cf1c97085e5bad
Amarjit Singh <amarjitmangarhia@gmail.com>
2023-09-21 12:59:18 -0400

    jj

--- src/components/Default/Default.js
+++ src/components/Default/Default.js
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import Slider from "../Slider/Slider";
 import { useSelector } from "react-redux";
 import Header from "../Header/Header";
+import classes from "../Default/Default.module.css"
+import stars from "../../img/star.png"
 
 
 function Default() {
@@ -14,21 +16,78 @@ function Default() {
 
     product.map((prod) => {
 
-        if(prod.id === params.id) {
+        if (prod.id === params.id) {
             productToUse.push(prod)
             console.log(productToUse[0])
         }
-        
+
     })
 
     return (
         <>
-        <Header />
-        <Slider />
-        <h1>{productToUse[0].header}</h1>
-        <img style={{width: "400px"}} src={productToUse[0].image} alt={productToUse[0].altTag} />
+            <Header />
+            <Slider />
+            <div className={classes.content}>
+                <div className={classes.img}>
+                    <img style={{ width: "400px" }} src={productToUse[0].image} alt={productToUse[0].altTag} />
+                </div>
+                <div className={classes.productData}>
+                    <h1>{productToUse[0].header}</h1>
+                    <div className={classes.price}>$450</div>
+                    <div className={classes.rating}>
+                        <div style={{width: "70px"}} className={classes.stars}><img src={stars} alt="ratings" /></div>
+                        <div className={classes.ratingNumbers}>359 rating</div>
+                    </div>
+
+                    <table className={classes.table}>
+                            <tr>
+                                <th>Brand</th>
+                                <td>Asus</td>
+                            </tr>
+                            <tr>
+                                <th>CPU socket</th>
+                                <td>Socket AM4</td>
+                            </tr>
+                            <tr>
+                                <th>Compatible devices</th>
+                                <td>Personal Computer</td>
+                            </tr>
+                            <tr>
+                                <th>RAM memory technology</th>
+                                <td>DDR4</td>
+                            </tr>
+                            <tr>
+                                <th>Compatible processors</th>
+                                <td>AMD 3rd Generation Ryzen</td>
+                            </tr>
+                            <tr>
+                                <th>Chipset type</th>
+                                <td>AMD A520</td>
+                            </tr>
+                            <tr>
+                                <th>Memory clock speed</th>
+                                <td>4400 MHz</td>
+                            </tr>
+                    </table>
+
+                    <div className={classes.aboutTheItem}>
+                        <h3>About this item</h3>
+                        <ul>
+                            <li>AMD AM4 Socket: Design for 3rd Gen AMD Ryzen and 3rd Gen AMD Ryzen with Radeon Graphics Processors</li>
+                            <li>Comprehensive cooling: VRM heatsink, PCH heatsink, and Fan Xpert 2+</li>
+                            <li>Fast Connectivity: 32Gbps M.2 slot, 1Gb Ethernet, USB 3.2 Gen 1 Type-A ports, plus HDMI 2.1 4K@60HZ, D-Sub, and DP outputs</li>
+                            <li>ASUS CSM motherboard: ASUS Corporate Stable Model with up to 36-month supply, EOL notice, and ECN control</li>
+                            <li>ASUS Control Center Express(ACCE) to make IT endpoint management simpler</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.buttons}>
+                <button>Buy Now</button>
+                <button>Add Cart</button>
+            </div>
         </>
     )
 }
 
-export default  Default;
+export default Default;
--- src/data-file.js
+++ src/data-file.js
@@ -4,6 +4,7 @@ const products = [
     header: "Asus Prime Gaming Motherboard",
     image: "https://m.media-amazon.com/images/I/61shsB-l2UL._SX569_.jpg",
     altTag: "asus-motherboard",
+
   },
   {
     id: "b",
@@ -42,10 +43,46 @@ const products = [
 
   {
     id: "g",
-    header: "KOORUI 24-Inch Curved Computer Monitor",
+    header: "KOORUI 24-Inch Curved Monitor",
     image: "https://m.media-amazon.com/images/I/71O6kpwBMyL._AC_SL1500_.jpg",
     altTag: "koorui-24-inch-curved-monitor",
   },
-];
+
+
+  {
+    id: "h",
+    header: "Skytech PC Desktop",
+    image: "https://m.media-amazon.com/images/I/81gqqKQVkAL._AC_SL1500_.jpg",
+    altTag: "Skytech-pc-desktop",
+  },
+
+  {
+    id: "i",
+    header: "Fanless Mini PC",
+    image: "https://m.media-amazon.com/images/I/712J2waX09L._AC_SL1500_.jpg",
+    altTag: "fanless-mini-pc",
+  },
+
+  {
+    id: "j",
+    header: "Logitech Wireless Keyboard",
+    image: "https://m.media-amazon.com/images/I/61N8p4GkUcL._AC_SL1500_.jpg",
+    altTag: "logitech-wireless-keyboard",
+  },
+
+  {
+    id: "k",
+    header: "Rapoo Wireless Gaming Mouse",
+    image: "https://m.media-amazon.com/images/I/61zVCmXt5AL._AC_SL1500_.jpg",
+    altTag: "gaming-mouse",
+  },
+
+  {
+    id: "l",
+    header: "SAMSUNG Portable SSD",
+    image: "https://m.media-amazon.com/images/I/81ngYSjZePL._AC_SL1500_.jpg",
+    altTag: "samsung-ssd",
+  },
+]
 
 export default products;
