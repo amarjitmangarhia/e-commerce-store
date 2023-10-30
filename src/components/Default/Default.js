492752e86eea292f16d24dac44fac8180eed520e
Amarjit Singh <amarjitmangarhia@gmail.com>
2023-10-30 15:46:34 -0400

    updated data file

--- src/components/Default/Default.js
+++ src/components/Default/Default.js
@@ -3,7 +3,6 @@ import Slider from "../Slider";
 import { useSelector } from "react-redux";
 import Header from "../Header/Header";
 import classes from "../Default/Default.module.css"
-import stars from "../../img/star.png"
 
 
 function Default() {
@@ -16,14 +15,29 @@ function Default() {
     let productToUse = []
 
     product.map((prod) => {
-
         if (prod.id === params.id) {
             productToUse.push(prod)
         }
-
     })
 
-   
+    const printStars = () => {
+        console.log()
+        let totalStars = "";
+        let stars = productToUse[0].stars;
+
+        for (let i = 0; i < stars; i++) {
+            totalStars = totalStars + "*"
+        }
+        return totalStars;
+    }
+
+    const showButtons = () => {
+        return (
+            <>
+                <button>Buy Now</button>
+                <button>Add Cart</button></>
+        )
+    }
 
     return (
         <>
@@ -31,62 +45,29 @@ function Default() {
             <Slider />
             <div className={classes.content}>
                 <div className={classes.img}>
-                    <img style={{ width: "400px" }} src={productToUse[0].image} alt={productToUse[0].altTag} />
+                    <img src={productToUse[0].image} alt={productToUse[0].altTag} />
                 </div>
                 <div className={classes.productData}>
                     <h1>{productToUse[0].header}</h1>
                     <div className={classes.price}>${productToUse[0].price}</div>
                     <div className={classes.rating}>
-                        <div style={{ width: "70px" }} className={classes.stars}><img src={stars} alt="ratings" /></div>
-                        <div className={classes.ratingNumbers}>{productToUse[0].rating} rating</div>
+                        <div className={classes.stars}>{printStars()}</div>
+                        <div className={classes.ratingNumbers}>{productToUse[0].rating}  rating</div>
                     </div>
-
-                    <table className={classes.table}>
-                        <tr>
-                            <th>Brand</th>
-                            <td>Asus</td>
-                        </tr>
-                        <tr>
-                            <th>CPU socket</th>
-                            <td>Socket AM4</td>
-                        </tr>
-                        <tr>
-                            <th>Compatible devices</th>
-                            <td>Personal Computer</td>
-                        </tr>
-                        <tr>
-                            <th>RAM memory technology</th>
-                            <td>DDR4</td>
-                        </tr>
-                        <tr>
-                            <th>Compatible processors</th>
-                            <td>AMD 3rd Generation Ryzen</td>
-                        </tr>
-                        <tr>
-                            <th>Chipset type</th>
-                            <td>AMD A520</td>
-                        </tr>
-                        <tr>
-                            <th>Memory clock speed</th>
-                            <td>4400 MHz</td>
-                        </tr>
-                    </table>
 
                     <div className={classes.aboutTheItem}>
                         <h3>About this item</h3>
                         <ul>
-                            <li>AMD AM4 Socket: Design for 3rd Gen AMD Ryzen and 3rd Gen AMD Ryzen with Radeon Graphics Processors</li>
-                            <li>Comprehensive cooling: VRM heatsink, PCH heatsink, and Fan Xpert 2+</li>
-                            <li>Fast Connectivity: 32Gbps M.2 slot, 1Gb Ethernet, USB 3.2 Gen 1 Type-A ports, plus HDMI 2.1 4K@60HZ, D-Sub, and DP outputs</li>
-                            <li>ASUS CSM motherboard: ASUS Corporate Stable Model with up to 36-month supply, EOL notice, and ECN control</li>
-                            <li>ASUS Control Center Express(ACCE) to make IT endpoint management simpler</li>
+                            {productToUse[0].desc ? productToUse[0].desc.map((arr) => {
+                                return <li>{arr}</li>
+                            }) : "No Description added yet."}
                         </ul>
+
                     </div>
                 </div>
             </div>
             <div className={classes.buttons}>
-                <button>Buy Now</button>
-                <button>Add Cart</button>
+                {productToUse[0].desc ? showButtons() : " "}
             </div>
         </>
     )
--- src/data-file.js
+++ src/data-file.js
@@ -6,12 +6,30 @@ const products = [
     altTag: "asus-motherboard",
     price: 459,
     rating: 359,
+    stars: 3,
+    desc: [
+      "Intel LGA 1700 socket: Ready for 12th&13th Gen Intel Core processors, support PCIe 5.0,DDR5 and out of box Windows 11 ready",
+      "Enhanced Power Solution: 16+1 DrMOS, ProCool sockets, military-grade TUF components, and Digi+ VRM for maximum durability and performance",
+      "Comprehensive Cooling : VRM heatsink, PCH fanless heatsink, M.2 heatsink, hybrid fan headers and Fan Xpert 4 utility",
+      "Ultra-Fast Gaming Networking : WiFi 6 AX201 (802.11 ax), Intel I225-V 2.5Gb LAN, TUF LANGuard and TurboLAN technology",
+      "Fastest Connectivity: 4x M.2/NVMe SSD, Front panel USB 3.2 Gen 2 Type-C header, USB Gen 2x2 Type-C and Thunderbolt 4 (USB4)header",
+    ]
   },
   {
     id: "b",
     header: "AMD Ryzen 5 3600",
     image: "https://m.media-amazon.com/images/I/71WPGXQLcLL._SX425_.jpg",
     altTag: "amd-ryzen-5",
+    price: 459,
+    rating: 359,
+    stars: 5,
+    desc: [
+      "AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler, most advanced processor in the desktop PC gaming segment",
+      "4 2 GHz max Boost unlocked for overclocking 35 MB of game Cache DDR4 3200 support,Can deliver ultra-fast 100+ FPS performance in the world's most popular games",
+      "GIGABYTE B450M DS3H WIFI Supports AMD RYZEN 2nd generation/ Ryzen with Radeon Vega Graphics/ Athlon with Radeon Vega Graphics/ Ryzen 1st generation processors",
+      "Realtek GbE LAN with cFosSpeed, Ultra-fast PCIe Gen3 x4 M.2 with PCIe NVMe & SATA mode support, Intel Dual Band 802.11ac WIFI, CEC 2019 ready",
+      "Dual Channel Non-ECC Unbuffered DDR4, 4 DIMMs,Intel Dual Band 802.11AC Wi-Fi & BT 4.2,High quality Audio Capacitors and Audio Noise Guard design",
+    ]
    
   },
 
@@ -20,6 +38,15 @@ const products = [
     header: "Corsair Vengeance LPX 8GB RAM",
     image: "https://m.media-amazon.com/images/I/51qXHU7XItL._SX522_.jpg",
     altTag: "corasir-vegence",
+    price: 459,
+    rating: 359,
+    stars: 5,
+    desc: [
+      "Designed for high-performance overclocking",
+      "Designed for great looks",
+      "Performance and Compatibility. Fan Included: No",
+      "Low-profile heat spreader design"
+    ]
   },
 
   {
@@ -27,6 +54,9 @@ const products = [
     header: "Corsair 550 Watt Power Supply",
     image: "https://m.media-amazon.com/images/I/514x11FuqCL._SX522_.jpg",
     altTag: "corsair 550 watt power supply",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
   {
@@ -34,6 +64,9 @@ const products = [
     header: "Seagate 1 TB Internal HDD",
     image: "https://m.media-amazon.com/images/I/71WDpMvEHTL._SX522_.jpg",
     altTag: "Seagate-1-TB-Internal",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
   {
@@ -41,6 +74,9 @@ const products = [
     header: "WD Green SATA 240GB SSD",
     image: "https://m.media-amazon.com/images/I/71yOeCQVTPL._SX569_.jpg",
     altTag: "wd-green-sata-240gb",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
   {
@@ -48,6 +84,9 @@ const products = [
     header: "KOORUI 24-Inch Curved Monitor",
     image: "https://m.media-amazon.com/images/I/71O6kpwBMyL._AC_SL1500_.jpg",
     altTag: "koorui-24-inch-curved-monitor",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
 
@@ -56,6 +95,9 @@ const products = [
     header: "Skytech PC Desktop",
     image: "https://m.media-amazon.com/images/I/81gqqKQVkAL._AC_SL1500_.jpg",
     altTag: "Skytech-pc-desktop",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
   {
@@ -63,6 +105,9 @@ const products = [
     header: "Fanless Mini PC",
     image: "https://m.media-amazon.com/images/I/712J2waX09L._AC_SL1500_.jpg",
     altTag: "fanless-mini-pc",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
   {
@@ -70,6 +115,9 @@ const products = [
     header: "Logitech Wireless Keyboard",
     image: "https://m.media-amazon.com/images/I/61N8p4GkUcL._AC_SL1500_.jpg",
     altTag: "logitech-wireless-keyboard",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
   {
@@ -77,6 +125,9 @@ const products = [
     header: "Rapoo Wireless Gaming Mouse",
     image: "https://m.media-amazon.com/images/I/61zVCmXt5AL._AC_SL1500_.jpg",
     altTag: "gaming-mouse",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 
   {
@@ -84,6 +135,9 @@ const products = [
     header: "SAMSUNG Portable SSD",
     image: "https://m.media-amazon.com/images/I/81ngYSjZePL._AC_SL1500_.jpg",
     altTag: "samsung-ssd",
+    price: 459,
+    rating: 359,
+    stars: 3
   },
 ]
 
